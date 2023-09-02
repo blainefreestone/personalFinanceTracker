debaa3dd01410dd8c436cebcea35486c51d1e7bd
blainefreestone <blainefreestone01@gmail.com>
2023-09-02 02:05:57 -0700

    readPayStub() now reads and sets all member variables from the pay stub except the document date. This method however, still needs commenting. The class as a whole also needs to be documented.

--- INOPayStub.py
+++ INOPayStub.py
@@ -1,26 +1,14 @@
 import datetime, fitz
 
-'''
-Members:
-filepath:str
-startDate:datetime
-endDate:datetime
-hours:float
-rate:float
-preDeductionPay:float
-deductions:dict(name:str, amount:float)
-netPay:float
-
-Methods:
-readPayStub()
-'''
-
 class INOPayStub:
     
     def __init__(self, filepath):
         self.__filepath = filepath
         self.readPayStub()
     
+    def __str__(self):
+        return f"Filepath: {self.__filepath}\nPre Deduction Pay: {self.__preDeductionPay}\nDeductions: {self.__deductions}\nNet Pay: {self.__netPay}\nStart Date: {self.__startDate}\nEnd Date: {self.__endDate}\nRate: {self.__rate}\nHours: {self.__hours}\n"
+
     def readPayStub(self):
         BLOCK_NO = 5
         BLOCK_TEXT = 4
@@ -36,11 +24,28 @@ class INOPayStub:
         payStubPdfObj = fitz.open(self.__filepath)
         for page in payStubPdfObj:
             blocks = page.get_text("blocks")
-            
-            for block in blocks:
-                if block[BLOCK_NO] not in (12, 21, 22, 27, 29, 31, 35, 39):
-                    continue
 
-                print(f"{block[BLOCK_NO]}) {block[BLOCK_TEXT].strip()}")
+            self.__preDeductionPay = float(blocks[PREDEDUCTIONPAY][BLOCK_TEXT].strip())
             
+            self.__netPay = float(blocks[NETPAY][BLOCK_TEXT].strip())
+
+            datesString = blocks[DATES][BLOCK_TEXT].strip()
+            dates = datesString.split(' - ')
+            self.__startDate = datetime.datetime.strptime(dates[0], "%m/%d/%Y")
+            self.__endDate = datetime.datetime.strptime(dates[1], "%m/%d/%Y")
+
+            self.__rate = float(blocks[RATE][BLOCK_TEXT].strip())
+
+            self.__hours = float(blocks[HOURS][BLOCK_TEXT].strip())
+
+            self.__deductions = {}
+            deductionNames = list(filter(None, blocks[DEDUCTIONSNAME][BLOCK_TEXT].split("\n")))
+            deductionAmounts = list(filter(None, blocks[DEDUCTIONSAMOUNT][BLOCK_TEXT].split("\n")))
+            assert len(deductionAmounts) == len(deductionNames), "deductionAmountsAndNamesDoNotMatch"
+            deductionsIndex = 0
+            while (deductionsIndex < len(deductionAmounts)):
+                self.__deductions[deductionNames[deductionsIndex].strip()] = float(deductionAmounts[deductionsIndex].strip())
+                deductionsIndex += 1
+
 payStub = INOPayStub("payStub.pdf")
+print(payStub)
